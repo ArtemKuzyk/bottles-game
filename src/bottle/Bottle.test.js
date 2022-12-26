7bd9911145008aed86bc22aedaba502a65fad013
Artem_Kuzyk <38781116+ArtemKuzyk@users.noreply.github.com>
2022-12-26 22:48:24 +0200

    Delete src/bottle directory

--- src/bottle/Bottle.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Bottle from './bottle';
-
-test('renders learn react link', () => {
-  render(<Bottle />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
